20903f679a7380b1b90ace3040da91d723b21eec
Cjota221 <Carolineazevedo075@gmail.com>
2025-06-03 13:24:35 -0300

    Create calcularFrete.js

---
+++ src/utils/calcularFrete.js
@@ -0,0 +1,18 @@
+// src/utils/calcularFrete.js
+export const calcularFreteBase = (peso, distancia) => {
+  // Lógica será implementada depois
+  // Exemplo simples:
+  // const taxaBase = 5;
+  // const taxaPorKg = 0.5;
+  // const taxaPorKm = 0.1;
+  // return taxaBase + (peso * taxaPorKg) + (distancia * taxaPorKm);
+  return 0;
+};
+
+export const adicionarTaxaUrgencia = (freteBase, taxaUrgenciaPercentual) => {
+  // Lógica será implementada depois
+  // return freteBase * (1 + taxaUrgenciaPercentual / 100);
+  return freteBase;
+};
+
+// Outras funções de cálculo de frete
